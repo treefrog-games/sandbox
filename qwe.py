3f23db050f18ef04cbdb47aca71605bbab99d5d6
SubMax <subbbbbbbbject@gmail.com>
2020-06-25 22:24:27 +0500

    test from Max2

--- qwe.py
+++ qwe.py
@@ -1,11 +1,1 @@
-from kivymd.app import MDApp
-from kivymd.uix.label import MDLabel
-from kivymd.uix.button import MDRoundFlatButton
-
-
-class MainApp(MDApp):
-    def build(self):
-        return MDLabel(text="Hello, World", halign="center")
-
-
-MainApp().run()
+#from kivymd.app import MDApp
